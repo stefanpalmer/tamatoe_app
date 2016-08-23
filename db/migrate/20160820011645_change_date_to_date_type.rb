15b4912d63d0d7df532052c2bc3da56ee0e8da59
Stefan <palmerstefan@yahoo.com>
2016-08-22 19:15:00 -0600

    Changed date migration

--- db/migrate/20160820011645_change_date_to_date_type.rb
+++ db/migrate/20160820011645_change_date_to_date_type.rb
@@ -1,5 +1,5 @@
 class ChangeDateToDateType < ActiveRecord::Migration[5.0]
   def change
-    change_column :entries, :date, :date
+    change_column :entries, :date, 'date USING date::date'
   end
 end
